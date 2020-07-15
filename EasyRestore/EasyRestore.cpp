5af8934f8f60c12d93787b2f477e7a18778b47ad
wooozie69 <54273457+wooozie69@users.noreply.github.com>
2020-07-15 17:16:27 +0300

    Correcting file paths
    Now getting desktop path dynamically, which what it should've been in the first place

--- EasyRestore/EasyRestore.cpp
+++ EasyRestore/EasyRestore.cpp
@@ -1,19 +1,20 @@
 ﻿#include <iostream>
 #include <string>
-#include <Windows.h>
-#include <shellapi.h>
 #include <fstream>
 #include <stdio.h>
 #include <urlmon.h>
+#include <shellapi.h>
 #include "firmware.cpp"
+#include "file_operations.cpp"
+
 #pragma comment(lib, "urlmon.lib")
 
 using namespace std;
 
 const string futurerestore_url = "https://github.com/s0uthwest/futurerestore/releases/download/245/futurerestore_win64_v245.zip";
-const string zip_file = "C:\\Users\\Ev\\Desktop\\fr.zip";
-const string futurerestore = "C:\\Users\\Ev\\Desktop\\futurerestore.exe";
-const string ipsw = "C:\\Users\\Ev\\Desktop\\ipsw.ipsw";
+const string zip_file = desktop + "\\fr.zip";
+const string futurerestore = desktop + "\\futurerestore.exe";
+const string ipsw = desktop + "\\ipsw.ipsw";
 
 inline void remove_leftover(string file) {
     if (remove(file.c_str()) == 0) cout << "Leftovers are removed successfully!" << endl;
@@ -61,14 +62,14 @@ inline void download(const string& version) {
 }
 
 inline void prep(string _sig_hash, string _fw_file, const string& version) {
-    ofstream exec("C:\\Users\\Ev\\Desktop\\run.bat");
-    exec << "cd C:\\Program Files\\7-Zip" << "\n7z e " << "\"" << zip_file << "\"" << " -o\"C:\\Users\\Ev\\Desktop\\\"";
+    ofstream exec(desktop + "\\run.bat");
+    exec << "cd C:\\Program Files\\7-Zip" << "\n7z e " << "\"" << zip_file << "\"" << " -o\\" << desktop << "\"";
     if (!(version == "10.2" || version == "10.2.1" || version == "10.3" || version == "10.3.1" || version == "10.3.2" || version == "10.3.3")) {
-        exec << "\ncd C:\\Users\\Ev\\Desktop\\" << "\nfuturerestore.exe -t " << _sig_hash << " --latest-sep --latest-baseband " << _fw_file << "\npause";
+        exec << "\ncd "<< desktop << "\nfuturerestore.exe -t " << _sig_hash << " --latest-sep --latest-baseband " << _fw_file << "\npause";
         exec.close();
     }
     else {
-        exec << "\ncd C:\\Users\\Ev\\Desktop\\" << "\nfuturerestore.exe -t " << _sig_hash << " -b baseband.bbfw -p buildmanifest.plist -s sep.im4p -m buildmanifest.plist ";
+        exec << "\ncd " << desktop << "\nfuturerestore.exe -t " << _sig_hash << " -b baseband.bbfw -p buildmanifest.plist -s sep.im4p -m buildmanifest.plist ";
         exec << _fw_file << "\npause";
         exec.close();
     }
@@ -77,17 +78,18 @@ inline void prep(string _sig_hash, string _fw_file, const string& version) {
 }
 
 inline void run() {
+    string run = desktop + "\\run.bat";
     ShellExecuteA(
         0,
         "open",
-        "C:\\Users\\Ev\\Desktop\\run.bat",
+        run.c_str(),
         NULL,
         NULL,
         SW_SHOW);
     system("pause");
-    remove_leftover("C:\\Users\\Ev\\Desktop\\run.bat");
-    remove_leftover("C:\\Users\\Ev\\Desktop\\fr.zip");
-    remove_leftover("C:\\Users\\Ev\\Desktop\\futurerestore.exe");
+    remove_leftover(run);
+    remove_leftover(zip_file);
+    remove_leftover(futurerestore);
 }
 
 int main()
