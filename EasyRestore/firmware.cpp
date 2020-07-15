390bcf38ec8a806278c254ad125e95e9ef20502e
wooozie69 <54273457+wooozie69@users.noreply.github.com>
2020-07-15 20:53:57 +0300

    Trying to fix Buildmanifest extracting
    Buildmanifest extracting fixed, still unable to download SEP and (probably) baseband

--- EasyRestore/firmware.cpp
+++ EasyRestore/firmware.cpp
@@ -35,6 +35,9 @@ const string iOS_12_4_5 = "http://updates-http.cdn-apple.com/2020WinterFCS/fullr
 const string iOS_12_4_6 = "http://updates-http.cdn-apple.com/2020WinterFCS/fullrestores/061-73609/7E12EF2A-B70C-4327-9EAC-5C28D62EA6AD/iPhone_4.0_64bit_12.4.6_16G183_Restore.ipsw";
 const string iOS_12_4_7 = "http://updates-http.cdn-apple.com/2020SpringFCS/fullrestores/061-94832/B6D93224-1059-4DF0-9438-78CD3BED57FE/iPhone_4.0_64bit_12.4.7_16G192_Restore.ipsw";
 
+const string bb_link = "https://raw.githubusercontent.com/wooozie69/BlueSt4r/master/iOS%2010%20SEP%20%2B%20BB/Mav7Mav8-7.60.00.Release.bbfw";
+const string sep_link = "https://raw.githubusercontent.com/wooozie69/BlueSt4r/master/iOS%2010%20SEP%20%2B%20BB/sep-firmware.n53.RELEASE.im4p";
+
 const string desktop = desktop_file_path();
 
 bool file_exist(const string& file) {
@@ -45,71 +48,108 @@ bool file_exist(const string& file) {
 void version_control(const string& version) {
 	if (version == "10.2" || version == "10.2.1" || version == "10.3" ||
 		version == "10.3.1" || version == "10.3.2" || version == "10.3.3") {
-		cout << "Since you're restoring to 10.2 - 10.3.3, some additional files are needed." << endl;
+		cout << "\nBefore starting, make sure to have iTunes installed on your computer (Not the one from MS Store)";
+		cout << "Since you're restoring to 10.2 - 10.3.3, some additional files will be downloaded." << endl;
+		cout << "Don't forget that this process is going to completely RESET your device, so make sure to back your data up first!";
+		cout << "\n\nWhen you're ready to begin, just press enter on your keyboard.." << endl;
+		system("pause");
 		ostream flush();
 
-		const string buildmanifest_link = "https://gitlab.com/devluke/stablea7/raw/master/A7_10.3.3_OTA_Manifests.zip";
-		wstring download_buildmanifest = wstring(buildmanifest_link.begin(), buildmanifest_link.end());
-		LPCWSTR url = download_buildmanifest.c_str();
+		if (file_exist(desktop + "\\buildmanifest.plist") == false) {
+			const string buildmanifest_link = "https://gitlab.com/devluke/stablea7/raw/master/A7_10.3.3_OTA_Manifests.zip";
+			wstring download_buildmanifest = wstring(buildmanifest_link.begin(), buildmanifest_link.end());
+			LPCWSTR url = download_buildmanifest.c_str();
 
-		const string destination = desktop + "\\buildmanifest_zip.zip";
-		wstring location = wstring(destination.begin(), destination.end());
-		LPCWSTR download_location = location.c_str();
+			const string destination = desktop + "\\buildmanifest_zip.zip";
+			wstring location = wstring(destination.begin(), destination.end());
+			LPCWSTR download_location = location.c_str();
 
-		HRESULT hr = URLDownloadToFile(NULL, url, download_location, 0, NULL);
-		if (file_exist(destination) == false) {
-			cerr << "Could not download buildmanifest file required for restoring" << endl;
-			system("pause");
-			exit(ERROR);
-		}
-		else {
-			ofstream temp(desktop + "\\extract_plist.bat");
-			temp << "mkdir" << desktop << "\\files\\" << endl;
-			temp << "cd C:\\Program Files\\7-Zip" << "\n7z e"<< desktop << "\\buildmanifest_zip.zip -o" << desktop << "\\files\\" << endl;
-			temp << "cd" << desktop << "\\files\\" << endl;
-			temp << "move BuildManifest_iPhone6,2_1033_OTA.plist" << desktop << "\\buildmanifest.plist" << endl;
-			temp << "cd " << desktop << "\\" << endl;
-			temp << "rd /S /Q files\\" << endl;
-			temp << "del buildmanifest_zip.zip" << endl;
-			temp.close();
+			cout << "Downloading BuildManifest.. " << endl;
+			HRESULT hr = URLDownloadToFile(NULL, url, download_location, 0, NULL);
+			if (file_exist(desktop + "\\buildmanifest_zip.zip") == false) {
+				cerr << "Could not download BuildManifest file required for restore process, aborting.. " << endl;
+				system("pause");
+				exit(ERROR);
+			}
+			else {
+				cout << "Grabbing the correct BuildManifest file, please DON'T touch anything.. " << endl;
+				ofstream temp(desktop + "\\extract_plist.bat");
+				temp << "mkdir " << desktop << "\\files\\" << endl;
+				temp << "cd C:\\Program Files\\7-Zip" << "\n7z e " << desktop << "\\buildmanifest_zip.zip -o" << desktop << "\\files\\" << endl;
+				temp << "cd " << desktop << "\\files\\" << endl;
+				temp << "move \"BuildManifest_iPhone6,2_1033_OTA.plist\" \"" << desktop << "\\buildmanifest.plist\"" << endl;
+				temp << "cd " << desktop << "\\" << endl;
+				temp << "rd /S /Q files\\" << endl;
+				temp << "del buildmanifest_zip.zip" << endl;
+				temp.close();
+
+				string* file = new string(desktop + "\\extract_plist.bat");
+				ShellExecuteA(
+					0,
+					"open",
+					file->c_str(),
+					NULL,
+					NULL,
+					SW_HIDE);
+
+				remove(file->c_str());
+				delete file;
+			}
 			ostream flush();
-
-			string* file = new string(desktop + "\\extract_plist.bat");
-			ShellExecuteA(
-				0,
-				"open",
-				file->c_str(),
-				NULL,
-				NULL,
-				SW_HIDE);
-
-			remove(file->c_str());
-			delete file;
 		}
-		ostream flush();
+		else cout << "BuildManifest already exists on Desktop, continuing.. " << endl;
 
-		const string sep_link = "https://raw.githubusercontent.com/wooozie69/BlueSt4r/master/iOS%2010%20SEP%20%2B%20BB/sep-firmware.n53.RELEASE.im4p";
-		const string bb_link = "https://raw.githubusercontent.com/wooozie69/BlueSt4r/master/iOS%2010%20SEP%20%2B%20BB/Mav7Mav8-7.60.00.Release.bbfw";
-		LPCWSTR sep_dl = wstring(sep_link.begin(), sep_link.end()).c_str();
-		LPCWSTR bb_dl = wstring(bb_link.begin(), bb_link.end()).c_str();
+		// Problems with downloading SEP (Probably Baseband too). Going to sort it out
+		if (file_exist(desktop + "\\sep.im4p") == false) {
+			LPCWSTR sep_dl = wstring(sep_link.begin(), sep_link.end()).c_str();
+			const string sep_file = desktop + "\\sep.im4p";
+			LPCWSTR sep_destination = wstring(sep_file.begin(), sep_file.end()).c_str();
 
-		const string sep_file = desktop + "\\sep.im4p";
-		const string bb_file = desktop + "\\baseband.bbfw";
-		LPCWSTR sep_destination = wstring(sep_file.begin(), sep_file.end()).c_str();
-		LPCWSTR bb_destination = wstring(bb_file.begin(), bb_file.end()).c_str();
-
-		HRESULT res_sep = URLDownloadToFile(NULL, sep_dl, sep_destination, 0, NULL);
-		if (!SUCCEEDED(res_sep)) {
-			cerr << "Downloading SEP failed, aborting.." << endl;
-			system("pause");
-			exit(ERROR);
+			cout << "Downloading SEP.." << endl;
+			HRESULT res_sep = URLDownloadToFile(NULL, sep_dl, sep_destination, 0, NULL);
+			if (file_exist(desktop + "\\sep.im4p") == false) {
+				cerr << "Downloading SEP failed, aborting.." << endl;
+				system("pause");
+				exit(ERROR);
+			}
 		}
-		HRESULT res_bb = URLDownloadToFile(NULL, bb_dl, bb_destination, 0, NULL);
-		if (!SUCCEEDED(res_bb)) {
-			cerr << "Downloading baseband failed, aborting.." << endl;
-			system("pause");
-			exit(ERROR);
+		else cout << "SEP already exists on Desktop, continuing.. " << endl;
+
+		if (file_exist(desktop + "\\baseband.bbfw") == false) {
+			LPCWSTR bb_dl = wstring(bb_link.begin(), bb_link.end()).c_str();
+			const string bb_file = desktop + "\\baseband.bbfw";
+			LPCWSTR bb_destination = wstring(bb_file.begin(), bb_file.end()).c_str();
+
+			cout << "Downloading Baseband.." << endl;
+			HRESULT res_bb = URLDownloadToFile(NULL, bb_dl, bb_destination, 0, NULL);
+			if (!SUCCEEDED(res_bb)) {
+				cerr << "Downloading baseband failed, aborting.." << endl;
+				system("pause");
+				exit(ERROR);
+			}
 		}
+		else cout << "Baseband already exists on Desktop, continuing.. " << endl;
+	}
+	else if (!(version == "11.3" || version == "11.3.1" || version == "11.4" || version == "11.4.1" ||
+		version == "12.0" || version == "12.0.1" || version == "12.1" || version == "12.1.1" ||
+		version == "12.1.2" || version == "12.1.3" || version == "12.1.4" || version == "12.2" ||
+		version == "12.3" || version == "12.3.1" || version == "12.4" || version == "12.4.1" ||
+		version == "12.4.2" || version == "12.4.3" || version == "12.4.4" || version == "12.4.5" ||
+		version == "12.4.6" || version == "12.4.7")) {
+		cerr << "Invalid iOS version, aborting.." << endl;
+		system("pause");
+		exit(ERROR);
+	}
+	else {
+		cout << "Restore will be processed with latest SEP and Baseband available" << endl;
+		cout << "If you're restoring to 11.3 - 11.4.1 or 12.2 - 12.4.7, restore should be fine";
+		cout << "\nBefore starting, make sure to have iTunes installed on your computer (Not the one from MS Store)";
+		cout << "\n\nWARNING: iOS 12.0 - 12.1.4 versions with latest SEP and Baseband have the Fortnight bug, ";
+		cout << "if you're NOT aware of this and trying to restore to iOS 12.0 - 12.1.4, you should either reconsider it or ";
+		cout << "make sure to NOT setup Touch ID and Passcode at initial setup of your iPhone after restoring it.";
+		cout << "\nDon't forget that this process is going to completely RESET your device, so make sure to back your data up first!";
+		cout << "\n\nWhen you're ready to begin, just press enter on your keyboard.." << endl;
+		system("pause");
 	}
 }
 
@@ -147,7 +187,10 @@ string get_firmware_link(const string& version) {
 		getline(cin, build_num);
 		if (build_num == "16C101")		return iOS_12_1_2_101;
 		else if (build_num == "16C104")		return iOS_12_1_2_104;
-		else					return "Invalid build number!";
+		else {
+			cerr << "Invalid build number, quitting.. " << endl;
+			system("pause");
+			exit(ERROR);
+		}
 	}
-	else						return "Unsupported version requested!";
 }
