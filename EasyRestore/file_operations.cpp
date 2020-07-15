8d6c750fecda7c40936c9c65dfda78af6f86192c
wooozie69 <54273457+wooozie69@users.noreply.github.com>
2020-07-15 20:52:54 +0300

    Moved file paths

--- EasyRestore/file_operations.cpp
+++ EasyRestore/file_operations.cpp
@@ -3,6 +3,13 @@
 
 using namespace std;
 
+const string futurerestore = desktop + "\\futurerestore.exe";
+const string ipsw = desktop + "\\ipsw.ipsw";
+const string signature_hash = desktop + "\\blob.shsh2";
+const string bb = desktop + "\\baseband.bbfw";
+const string sep = desktop + "\\sep.im4p";
+const string buildmanifest = desktop + "\\buildmanifest.plist";
+
 string desktop_file_path() {
     wchar_t* desktop = nullptr;
     HRESULT hr = SHGetKnownFolderPath(
