afcb1ccd12fa757322c467bb1c50a4f07850cec2
wooozie69 <54273457+wooozie69@users.noreply.github.com>
2020-07-16 12:28:02 +0300

    Cleanup method added

--- EasyRestore/file_operations.cpp
+++ EasyRestore/file_operations.cpp
@@ -9,6 +9,8 @@ const string signature_hash = desktop + "\\blob.shsh2";
 const string bb = desktop + "\\baseband.bbfw";
 const string sep = desktop + "\\sep.im4p";
 const string buildmanifest = desktop + "\\buildmanifest.plist";
+const string zip_file = desktop + "\\fr.zip";
+const string run_path = desktop + "\\run.bat";
 
 string desktop_file_path() {
     wchar_t* desktop = nullptr;
@@ -28,4 +30,17 @@ string desktop_file_path() {
         system("pause");
         exit(ERROR);
     }
+}
+
+void delete_file(const string& file) {
+    if (remove(file.c_str()) == 0) cout << "Leftover removed successfully!" << endl;
+    else cout << "Deleting leftover failed! You may need to delete them manually from your Desktop." << endl;
+}
+
+void check_leftovers() {
+    if (file_exist(sep)) delete_file(sep);
+    if (file_exist(bb)) delete_file(bb);
+    if (file_exist(buildmanifest)) delete_file(buildmanifest);
+    if (file_exist(futurerestore)) delete_file(futurerestore);
+    if (file_exist(run_path)) delete_file(run_path);
 }
