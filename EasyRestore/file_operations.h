8596443e7361066be7a2b5ed00cad01a1c784bc2
wooozie69 <54273457+wooozie69@users.noreply.github.com>
2020-07-16 12:26:25 +0300

    Tidying headers up

--- EasyRestore/file_operations.h
+++ EasyRestore/file_operations.h
@@ -4,9 +4,12 @@
 #include <tchar.h>
 #include <ShlObj.h>
 #include <Windows.h>
+#include <fstream>
 #include <string>
 
 string desktop_file_path();
+void delete_file(const string& file);
+void check_leftovers();
 
 #endif // !file_operations
 
